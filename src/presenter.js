14867ae653507eee9dfcb9fe420801f4a2f68281
Mateo Wilson <mat21quisp@gmail.conm>
2026-09-09 09:17:11 -0400

    feat: agregar seleccion de tipo de cliente en la UI y mostrar descuentos de envio y monto fijo en el detalle

--- src/presenter.js
+++ src/presenter.js
@@ -6,6 +6,7 @@ const cantidad = document.querySelector("#cantidad-input");
 const precio = document.querySelector("#precio-input");
 const estado = document.querySelector("#estado-select");
 const categoria = document.querySelector("#categoria-select");
+const tipoCliente = document.querySelector("#tipo-cliente-select");
 const peso = document.querySelector("#peso-input");
 const form = document.querySelector("#venta-form");
 const cancelarButton = document.querySelector("#cancelar-button");
@@ -18,6 +19,7 @@ function reiniciarFormulario() {
   precio.value = estadoInicial.precio;
   estado.value = estadoInicial.estado;
   categoria.value = "Varios";
+  tipoCliente.value = estadoInicial.tipoCliente;
   peso.value = 0;
   div.innerHTML = "";
 }
@@ -42,8 +44,14 @@ function renderizarCategorias() {
   categoria.value = "Varios";
 }
 
+function renderizarTiposCliente() {
+  tipoCliente.innerHTML = venta.obtenerTiposCliente().map((t) => "<option value='" + t + "'>" + t + "</option>").join("");
+  tipoCliente.value = venta.crearEstadoInicial().tipoCliente;
+}
+
 renderizarEstados();
 renderizarCategorias();
+renderizarTiposCliente();
 
 function renderizar() {
   const cantidadValor = Number(cantidad.value);
@@ -65,10 +73,16 @@ function renderizar() {
   const impuestoAdicional = venta.impuestoAdicionalCategoria(categoria.value);
   const descuentoCategoria = venta.descuentoAdicionalCategoria(categoria.value);
   const costoEnvioUnidad = venta.costoEnvioUnidad(Number(peso.value));
+  const costoEnvioTotal = venta.redondearMoneda(costoEnvioUnidad * cantidadValor);
+  const porcentajeDescuentoEnvio = venta.descuentoEnvioCliente(tipoCliente.value);
+  const descuentoEnvio = venta.redondearMoneda(costoEnvioTotal * porcentajeDescuentoEnvio);
+  const costoEnvioFinal = venta.redondearMoneda(costoEnvioTotal - descuentoEnvio);
+  const descuentoMontoFijo = venta.descuentoMontoFijo(subtotal, categoria.value, tipoCliente.value);
   const total = venta.redondearMoneda(venta.calcularTotalImpuesto(subtotal, impuesto));
   const descuento = venta.calcularDescuento(total);
   const ahorro = venta.redondearMoneda(venta.calcularAhorro(total, descuento));
   const totalFinal = venta.redondearMoneda(venta.calcularTotalFinal(subtotal, ahorro, impuesto));
+  const precioFinal = venta.redondearMoneda(totalFinal + costoEnvioFinal - descuentoMontoFijo);
 
   const lineas = [
     "Subtotal: " + subtotal,
@@ -76,11 +90,14 @@ function renderizar() {
     "Impuesto adicional (" + categoria.value + " " + impuestoAdicional + "): " + venta.redondearMoneda(impuestoAdicional * subtotal),
     "Descuento adicional (" + categoria.value + " " + descuentoCategoria + "): " + venta.redondearMoneda(descuentoCategoria * subtotal),
     "Costo de envio por unidad (peso " + peso.value + "): $" + venta.redondearMoneda(costoEnvioUnidad),
+    "Costo de envio total: $" + costoEnvioTotal,
+    "Descuento en costo de envio (" + tipoCliente.value + " " + porcentajeDescuentoEnvio + "): $" + descuentoEnvio,
+    "Descuento fijo por tipo de cliente: $" + descuentoMontoFijo,
     "Total: " + total,
     "Descuento: " + descuento,
     "Ahorro: $" + ahorro,
     "Total con descuento: " + venta.redondearMoneda(venta.calcularTotalConDescuento(total)),
-    "Precio final: " + totalFinal
+    "Precio final: " + precioFinal
   ];
   div.innerHTML = lineas.map((linea) => "<p>" + linea + "</p>").join("");
 }
